78fa637f9ec21dd171dbb78fd6b1e3cef9be965f
Elin <elinriihimaki@gmail.com>
2023-04-27 19:12:45 +0200

    login function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import axios from "axios";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <div>
+        {/*add in header here as header should always be visible in all routes!
+        but unsure how this works if we want different buttons? */}
+      </div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}/> 
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 

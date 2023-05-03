9166fbdd2e905b6f674bee8dbe4071e075b32afb
dxenia <marascadanaxenia@gmail.com>
2023-05-03 13:17:12 +0200

    imported useState, added values to form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,16 +1,25 @@
+import { useState } from "react";
+import axios from "axios";
+
+
 const Register = () => {
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="register-container">
       <form className="register-form">
         <h1>Register User</h1>
         <label>
-          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Name" value={username} />
         </label>
         <label>
-          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Email" value={email} />
         </label>
         <label>
-          <input type="text" placeholder="Password" />
+          <input type="text" placeholder="Password" value={password} />
         </label>
         <button className="register-btn">
           <p>Register</p>

0b4437bf84f16361207a9b90f2d290cb56881076
dxenia <marascadanaxenia@gmail.com>
2023-05-16 07:19:03 +0200

    matched login to sign up information

--- src/components/Register.js
+++ src/components/Register.js
@@ -5,23 +5,22 @@ import axios from "axios";
 const Register = () => {
 
   const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const checkEmail = (users) => {
-    const user = users.find((user) => user.email === email)
+  const checkUser = (users) => {
+    const user = users.find((user) => user.username === username)
     if (user) return user;
   };
 
   const handleSubmit = async () => {
     const user = await axios
       .get("http://localhost:6001/users")
-      .then((res) => checkEmail(res.data, email));
+      .then((res) => checkUser(res.data, username));
     
     if (user) {
       alert("User already exists!")
     } else {
-      const user = { username, email, password };
+      const user = { username, password };
       axios.post("http://localhost:6001/users", user).then(alert("User created!"));
     }
 
@@ -34,19 +33,10 @@ const Register = () => {
         <label className="register-label">
           <input
             type="text"
-            placeholder="Name"
+            placeholder="Username"
             className="register-input"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <label className="register-label">
-          <input
-            type="text"
-            placeholder="Email"
-            className="register-input"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
           />
         </label>
         <label className="register-label">

d37cf6e303d9ee56ff76588e6306ee2656a0359a
dxenia <marascadanaxenia@gmail.com>
2023-05-17 18:08:34 +0200

    styled navigation bar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,24 +2,17 @@ import {Link} from 'react-router-dom';
 
 const   NavBar= ()=>{
   return  (
-      <section className='navBar'>
- 
-    
-      
-      <h1>Quiz Game</h1>
-              
-              <nav className="navigation">
-                  
-                      <button type="submit" class="btn-log-in">
-                        
-                        <Link to='/Login'>Login</Link></button>
-                      <button type="submit" class="btn-Register"><Link to ='/Register'> Register</Link></button>
-              </nav>
-      
-          
-      </section>
-      
-      );
+    <section className='navBar'>
+      <h1 className="nav-heading">Quiz Game</h1>
+        <nav className="navigation">
+          <button type="submit" className="btn-log-in">
+            <Link to='/Login'>Login</Link>
+          </button>
+          <button type="submit" className="btn-Register">
+            <Link to='/Register'> Register</Link></button>
+        </nav>
+    </section>
+  );
 };
 
 

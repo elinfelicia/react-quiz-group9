e4fc1f16e5cccd0c0a8fb5385fb8cf9514c83876
sarawazir49 <123027465+sarawazir49@users.noreply.github.com>
2023-05-17 13:09:16 +0200

    navbar changes

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,32 +1,26 @@
 import {Link} from 'react-router-dom';
 
 const   NavBar= ()=>{
-    return  (
-<body>
-        
-            <input type="checkbox" id="check"></input>
-            <label for="check">
-            <i className="fa fa-bars" id="btn"></i>
-            <i className="fa fa-times" id="cancel"></i></label>
-            <div className="sidebar">
-                <header>Quiz Game</header>
-        <ul>
-        <li><Link className='links' to = "/Login"><i class="fa fa-sign-in fa-x">  Sign in </i>  </Link>
-          </li>
-            <li><Link className='links' to = "/Register"><i class="fa fa-user-plus">  Sign up </i>  </Link>
-          </li>
-          <li><Link className='links' to = "/quiz"> <i class="fa fa-hourglass-start">  Start quiz </i>  </Link>
-          </li>
-        
-            <li><a href="https://Facebook.com"><i class="fa fa-facebook-official"></i> Facebook</a></li>
-            <li><a href="https://instagram.com"><i class="fa fa-instagram"></i> Instagram</a></li>
-
-        </ul>
-            </div>
-</body>
-
-
-);
+  return  (
+      <section className='navBar'>
+ 
+    
+      
+      <h1>Quiz Game</h1>
+              
+              <nav className="navigation">
+                  
+                      <button type="submit" class="btn-log-in">
+                        
+                        <Link to='/Login'>Login</Link></button>
+                      <button type="submit" class="btn-Register"><Link to ='/Register'> Register</Link></button>
+              </nav>
+      
+          
+      </section>
+      
+      );
 };
+
 
 export default NavBar;
